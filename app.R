79ce63dde9bd40704455bfbeae25900113095188
Frederikke Lan <72468346+frederikkelan@users.noreply.github.com>
2023-11-08 20:45:38 +0100

    Added report for reading and downloading (#1)
    * added downloadTab
    
    * added report

--- app.R
+++ app.R
@@ -13,13 +13,13 @@ data <- read.csv("Earthquakes.csv", header=TRUE, sep=",")
 
 
 #UI
-ui <- navbarPage("Earthquakes from 1900 - 2013",
-                 tabPanel("Dataset",
+ui <- navbarPage("Group 14: Earthquakes from 1900 - 2013",
+                 tabPanel("Data set",
                           mainPanel(
                             DT::dataTableOutput("data")
                           )
-                          ),
-                 tabPanel("two")
+                 ),
+                 tabPanel("Report", tags$iframe(style = "height:600px; width:100%; scrolling=yes", src = "Report.pdf"))
                  )
 
 
