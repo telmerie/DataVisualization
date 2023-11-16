aefd5aa83acb18a8b7fe87918b33971be613b766
unknown <tes_7583@hotmail.com>
2023-11-16 09:35:14 +0100

    added map, not in app

--- app.R
+++ app.R
@@ -1,15 +1,30 @@
 #install packages
 install.packages("shiny")
 install.packages("DT")
+install.packages("tidyverse")
+install.packages("sf")
+install.packages("mapview")
+install.packages("leaflet")
 
 #import libraries
 library(shiny)
 library(DT)
+library(tidyverse)
+library(sf)
+library(mapview)
+library(leaflet)
+
+
 
 #import dataset
 path <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(path)
 data <- read.csv("Earthquakes.csv", header=TRUE, sep=",")
+
+world_map <-  mapview(data, xcol = "longitude", ycol = "latitude",crs = 4269, grid = FALSE)
+
+
+
 
 
 #UI
@@ -19,7 +34,14 @@ ui <- navbarPage("Earthquakes from 1900 - 2013",
                             DT::dataTableOutput("data")
                           )
                           ),
-                 tabPanel("two")
+                 tabPanel("two",
+                          mainPanel(
+                            
+                          )
+                          
+  
+                        
+                          )
                  )
 
 
@@ -27,6 +49,7 @@ ui <- navbarPage("Earthquakes from 1900 - 2013",
 server <- function(input, output){
   
   output$data = DT::renderDataTable({data})
+  renderLeaflet(mapview::mapview2leaflet(data, xcol = "longitude", ycol = "latitude",crs = 4269, grid = FALSE)) 
   
 }
   
