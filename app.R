ea5e0df06671c478e15596a81d5c613ca7efe2e3
unknown <tes_7583@hotmail.com>
2023-11-21 19:40:41 +0100

    map working

--- app.R
+++ app.R
@@ -1,10 +1,10 @@
 #install packages
-install.packages("shiny")
-install.packages("DT")
-install.packages("tidyverse")
-install.packages("sf")
-install.packages("mapview")
-install.packages("leaflet")
+#install.packages("shiny")
+#install.packages("DT")
+#install.packages("tidyverse")
+#install.packages("sf")
+#install.packages("mapview")
+#install.packages("leaflet")
 
 #import libraries
 library(shiny)
@@ -21,7 +21,8 @@ path <- dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(path)
 data <- read.csv("Earthquakes.csv", header=TRUE, sep=",")
 
-world_map <-  mapview(data, xcol = "longitude", ycol = "latitude",crs = 4269, grid = FALSE)
+
+#world_map <-  mapview(data, xcol = "longitude", ycol = "latitude",crs = 4269, grid = FALSE)
 
 
 
@@ -34,13 +35,19 @@ ui <- navbarPage("Earthquakes from 1900 - 2013",
                             DT::dataTableOutput("data")
                           )
                           ),
-                 tabPanel("two",
-                          mainPanel(
-                            
-                          )
-                          
-  
+                 tabPanel("World Map",
+                          sliderInput("years",
+                                      "Year:",
+                                      min = 1990, 
+                                      max = 2014,
+                                      value = 2000,
+                                      step = 1,
+                                      animate = TRUE
+                                      ),
                         
+                          fluidRow(
+                            leafletOutput("mapplot","100%"),
+                          )
                           )
                  )
 
@@ -49,8 +56,23 @@ ui <- navbarPage("Earthquakes from 1900 - 2013",
 server <- function(input, output){
   
   output$data = DT::renderDataTable({data})
-  renderLeaflet(mapview::mapview2leaflet(data, xcol = "longitude", ycol = "latitude",crs = 4269, grid = FALSE)) 
+  #renderLeaflet(mapview::mapview2leaflet(data, xcol = "longitude", ycol = "latitude",crs = 4269, grid = FALSE)) 
+
   
+ 
+  
+  output$mapplot <- renderLeaflet({
+    years <-  input$years 
+    
+    quakeYear <- dplyr::filter(data, grepl(years , Date))
+    
+   
+    m <- mapview(quakeYear, xcol = "longitude", ycol = "latitude",crs = 4269, grid = FALSE)
+    m@map
+  })
+  
+  
+
 }
   
 #Create app
